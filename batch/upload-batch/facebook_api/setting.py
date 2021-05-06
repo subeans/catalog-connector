0808249cdfe97f8e783812686d1153d2f08837b5
Subin <39542978+subeans@users.noreply.github.com>
2021-05-06 19:41:08 +0900

    remove info

--- batch/upload-batch/facebook_api/setting.py
+++ batch/upload-batch/facebook_api/setting.py
@@ -6,8 +6,8 @@ import json
 import pymongo
 
 
-MONGODB_USER = 'digester_prod'
-MONGODB_PASSWORD = 'qmfforvldzm'
+MONGODB_USER = '--'
+MONGODB_PASSWORD = '--'
 MONGODB_URI = 'mongodb://%s:%s@3.34.3.239:27017' % (MONGODB_USER, MONGODB_PASSWORD)
 MONGODB_DATABASE = 'prod'
 
