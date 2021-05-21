909cb12590fd1222cf13a6dcfea7d84472ab2856
Subin <39542978+subeans@users.noreply.github.com>
2021-05-22 02:53:23 +0900

    nope

--- batch/build_docker.sh
+++ batch/build_docker.sh
@@ -3,8 +3,8 @@ ecr="008747557116.dkr.ecr.ap-northeast-2.amazonaws.com"
 docker_name="catalog-connector"
 tag="latest"
 
-name="subeans"
-password="subin8860"
+name=""
+password=""
 
 aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${ecr}
 docker build --build-arg "NAME=${name}" \
